3daa598da4b79331d7ca55e524cd8da7599e116e
Codster dev <127374043+codster15@users.noreply.github.com>
2024-01-17 02:39:34 +0530

    Add files via upload

---
+++ thanksApp.js
@@ -0,0 +1,11 @@
+
+
+
+
+var dismis = document.querySelector(".li5");
+
+var DismissMSG = function(){
+    window.location.href = "index.html";   // Most Most  Important Line............... to link the file 
+}
+
+dismis.addEventListener("click" , DismissMSG);
